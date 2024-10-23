0ef13bc1757c6b5ef625616f6dadbd86d9a0582b
farizindrad <farizindra864207@gmail.com>
2024-10-23 08:21:24 +0700

    daily ngrok

--- src/components/CardCampaigns.tsx
+++ src/components/CardCampaigns.tsx
@@ -41,7 +41,7 @@ const CardCampaign: React.FC<CardCampaignProps> = ({
     const fetchTotalAmount = async () => {
       try {
         const response = await fetch(
-          `https://f2b2-110-136-78-231.ngrok-free.app/total-amount/${id}`,
+          `https://8d9e-110-136-78-231.ngrok-free.app/total-amount/${id}`,
           {
             method: "GET",
             headers: {
